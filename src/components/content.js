35b8e6b24a422f85e1c6fbaee5c2e863cc0af75f
maru2katabami <maru2katabami@gmail.com>
2025-03-13 22:26:28 +0900

    update

--- src/components/content.js
+++ src/components/content.js
@@ -1,19 +1,23 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const Content = () => {
 
+  const headerRef = useRef(null)
   const [space, setSpace] = useState(0)
 
   useEffect(() => {
     setSpace(Math.min(window.innerWidth % 369, 30))
+    console.log(headerRef)
+    headerRef.current.scrollLeft = headerRef.current.scrollWidth
   }, [])
 
   return (
     <div
       className="absolute top-0 p-5 size-full grid-line flex flex-wrap justify-between overflow-hidden">
       <div
+        ref={headerRef}
         className="absolute top-0 left-0 w-full h-56 flex items-center overflow-x-scroll"
         style={{paddingLeft: `${space/2}px`, paddingRight: `${space/2}px`, gap: `${space}px`}}>
         <div className="w-[369px] h-52 rounded-2xl bg-black shrink-0">
@@ -31,7 +35,12 @@ export const Content = () => {
 
         </div>
       </div>
-      <div></div>
+      <div className="absolute bottom-0 left-0 w-full h-[calc(100%-224px)]">
+        <img
+          className="w-40 h-20 rounded"
+          src="https://img.youtube.com/vi/G-_w5PDL9yw/maxresdefault.jpg"
+          alt="YouTube Thumbnail"/>
+      </div>
     </div>
   )
 }
