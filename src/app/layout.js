31940c826c3495d92df51288d03cefc746aa89b5
maru2katabami <maru2katabami@gmail.com>
2025-03-24 18:54:40 +0900

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,8 @@ export default function RootLayout({children}) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
       </head>
       <body>
         {children}
