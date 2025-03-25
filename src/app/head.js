cea88206fcd82053c14fe56f893adb00e0b16371
maru2katabami <maru2katabami@gmail.com>
2025-03-25 11:29:52 +0900

    update

--- src/app/head.js
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-    </>
-  )
-}
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import { Notification } from "@/cmp/notification"
 
 export default function Page() {
   return (
-    <main>
+    <main className="p-safe">
       <MiniPlay>
         <Notification/>
       </MiniPlay>
