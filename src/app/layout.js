712938caddcd7f51e6864d592befed3c3fe23de6
maru2katabami <maru2katabami@gmail.com>
2025-03-25 11:06:04 +0900

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import "./globals.css"
 
 export const metadata = {
@@ -8,11 +9,11 @@ export const metadata = {
 export default function RootLayout({children}) {
   return (
     <html lang="en">
-      <head>
+      <Head>
         <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover,user-scalable=no"/>
         <meta name="apple-mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent"/>
-      </head>
+      </Head>
       <body>
         {children}
       </body>
